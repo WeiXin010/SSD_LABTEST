2317e73618c3a33f007b33a3c6bed37ce9d6456c
Wei Xin <lwxin87@gmail.com>
2025-07-25 16:49:03 +0800

    eslint check

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,35 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+// eslint.config.js (flat config)
+import js from "@eslint/js"; // ESLint's built-in base rules
+import globals from "globals"; // Common global variables (like window, document)
+import { defineConfig } from "eslint/config"; // Flat config helper
+import pluginSecurity from "eslint-plugin-security";
+import pluginSecurityNode from "eslint-plugin-security-node";
+import pluginUnsantized from "eslint-plugin-no-unsanitized";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,             // ESLint core rules
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"], // Apply to all JS/JSX files
+    languageOptions: {
+      ecmaVersion: 2020, // Enable modern JavaScript features
+      sourceType: "module", // Support ESModules (i.e., `import/export`)
+      globals: globals.node, // Enable browser globals like `window`, `document`
+    },
+    plugins: {
+      js, // ESLint core rules
+      security: pluginSecurity,
+      "security-node": pluginSecurityNode,
+      "no-unsanitized": pluginUnsantized,
+    },
+    rules: {
+      // Start with recommended React rules
+      ...js.configs.recommended.rules,
+      ...pluginSecurity.configs.recommended.rules,
+      ...pluginSecurityNode.configs.recommended.rules,
+      ...pluginUnsantized.configs.recommended.rules,
+
+      "security/detect-eval-with-expression": "error",
+      "no-console": "warn",
+    },
+  },
 ]);
